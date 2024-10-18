aee767f7905b743aecdf3cbc59c961b259ad671a
Thiago Miranda <einsteinforcomputer@gmail.com>
2024-10-18 14:17:24 -0300

    feat: add npm publish

--- setup.js
+++ setup.js
@@ -1,15 +1,15 @@
 const { existsSync, readFileSync, writeFileSync } = require('fs');
 const { resolve } = require('path');
 
-const tailwindConfigPath = resolve(process.cwd(), 'tailwind.config.js');
-const nextConfigPath = resolve(process.cwd(), 'next.config.js');
+const tailwindConfigPath = resolve(process.cwd(), 'tailwind.config.ts');
+const nextConfigPath = resolve(process.cwd(), 'next.config.mjs');
 
 const updateTailwindConfig = () => {
   if (existsSync(tailwindConfigPath)) {
     const configContent = readFileSync(tailwindConfigPath, 'utf-8');
     const updatedConfig = configContent.replace(
       /content: \[(.*?)\]/s,
-      (match) => match.replace(']', `,\n    "./node_modules/@v0up3r/zt-ui/app/components/**/*.{js,ts,jsx,tsx,mdx}",\n]`)
+      (match) => match.replace(']', `"./node_modules/@v0up3r/zt-ui/app/components/**/*.{js,ts,jsx,tsx,mdx}"\n]`)
     );
 
     writeFileSync(tailwindConfigPath, updatedConfig, 'utf-8');
@@ -21,18 +21,44 @@ const updateTailwindConfig = () => {
 
 const updateNextConfig = () => {
   if (existsSync(nextConfigPath)) {
-    const configContent = readFileSync(nextConfigPath, 'utf-8');
-    const updatedConfig = configContent.replace(
-      /transpilePackages: \[(.*?)\]/s,
-      (match) => match.replace(']', `,\n    '@v0up3r/zt-ui',\n]`)
-    );
+    let configContent = readFileSync(nextConfigPath, 'utf-8');
 
-    writeFileSync(nextConfigPath, updatedConfig, 'utf-8');
-    console.log('next.config.js atualizado com a configuração da biblioteca.');
+    // Verifica se o transpilePackages já existe
+    if (/transpilePackages: \[(.*?)\]/s.test(configContent)) {
+      // Se já existir, só adiciona '@v0up3r/zt-ui' se não estiver presente
+      configContent = configContent.replace(
+        /transpilePackages: \[(.*?)\]/s,
+        (match, p1) => {
+          if (!p1.includes("'@v0up3r/zt-ui'")) {
+            return `transpilePackages: [${p1}, '@v0up3r/zt-ui']`;
+          }
+          return match; // Se já existir, mantém o original
+        }
+      );
+    } else {
+      // Se não existir, adiciona `transpilePackages` no final
+      configContent += `\n\n/** @type {import('next').NextConfig} */\nconst nextConfig = {\n  transpilePackages: ['@v0up3r/zt-ui'],\n};\n\nexport default nextConfig;\n`;
+    }
+
+    // Grava o conteúdo de volta ao arquivo, sem sobrescrever o restante
+    writeFileSync(nextConfigPath, configContent, 'utf-8');
+    console.log('next.config.mjs atualizado com a configuração da biblioteca.');
   } else {
-    console.error('next.config.js não encontrado.');
+    // Se o arquivo não existir, cria um novo arquivo com a configuração básica
+    const newConfig = `
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  transpilePackages: ['@v0up3r/zt-ui']
+};
+
+export default nextConfig;
+`;
+
+    writeFileSync(nextConfigPath, newConfig, 'utf-8');
+    console.log('next.config.mjs criado e configurado com sucesso.');
   }
 };
+
 
 updateTailwindConfig();
 updateNextConfig();
