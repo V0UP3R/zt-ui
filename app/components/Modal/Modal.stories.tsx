1de66e4aeb58fbb567cd9e52841476290cb1d7aa
Thiago Miranda <einsteinforcomputer@gmail.com>
2024-11-08 10:10:57 -0300

    fix: modal bugs in close

--- app/components/Modal/Modal.stories.tsx
+++ app/components/Modal/Modal.stories.tsx
@@ -1,41 +1,18 @@
 import React, { useState } from "react";
 import { Meta, StoryFn } from "@storybook/react";
-import Modal, { ModalProps } from "./Modal";
+import Modal from "./Modal"; // Importando o componente Modal atualizado
 import Button from "../Button/Button";
 
 export default {
   title: "Components/Modal/Modal",
-  component: Modal,
+  component: Modal.Root,
   argTypes: {
     isOpen: {
       control: "boolean",
       description: "Controla se o modal está aberto ou fechado",
     },
-    title: {
-      control: "text",
-      description: "Título exibido no cabeçalho do modal",
-    },
-    children: {
-      control: "text",
-      description: "Conteúdo do corpo do modal",
-    },
-    headerContent: {
-      control: "text",
-      description: "Conteúdo do cabeçalho do modal, caso desejado.",
-    },
-    footerContent: {
-      control: "text",
-      description: "Conteúdo do rodapé do modal, caso desejado.",
-    },
-    onClose: {
-      action: "onClose",
-      description: "Função chamada ao fechar o modal",
-    },
     size: {
-      control: {
-        type: "select",
-        options: ["sm", "md", "lg", "xl", "full"],
-      },
+      control: { type: "select", options: ["sm", "md", "lg", "xl", "full"] },
       description: "Tamanho do modal (sm, md, lg, xl, full)",
     },
     overflow: {
@@ -46,93 +23,80 @@ export default {
   },
 } as Meta;
 
-const Template: StoryFn<ModalProps> = (args: ModalProps) => {
+const Template: StoryFn = (args) => {
   const [isOpen, setIsOpen] = useState(false);
 
-  const handleOpen = () => setIsOpen(true);
-  const handleClose = () => setIsOpen(false);
+  const handleToggleModal = () => setIsOpen(!isOpen);
 
   return (
     <>
       <Button
-        onClick={handleOpen}
+        onClick={handleToggleModal}
         variant="fill"
         className="px-4 py-2 bg-blue-500 text-white rounded hover:bg-blue-600"
       >
-        Abrir Modal
+        {isOpen ? "Fechar Modal" : "Abrir Modal"}
       </Button>
-      <Modal {...args} isOpen={isOpen} onClose={handleClose} />
+      <Modal.Root
+        {...args}
+        isOpen={isOpen}
+        onClose={handleToggleModal}
+        toggleModalOpen={setIsOpen}
+      >
+        <Modal.Header>
+          <Modal.Title>Exemplo de Modal</Modal.Title>
+        </Modal.Header>
+        <Modal.Content>
+          <p>Este é o conteúdo do modal.</p>
+        </Modal.Content>
+        <Modal.Footer>
+          <Button
+            onClick={() => alert("Ação no rodapé!")}
+            variant="outline"
+            className="mt-4"
+          >
+            Ação no Rodapé
+          </Button>
+        </Modal.Footer>
+      </Modal.Root>
     </>
   );
 };
 
-// Exemplo base com botões para os diferentes tamanhos
-export const WithOpenButton = Template.bind({});
-WithOpenButton.args = {
-  title: "Modal Aberta",
-  children: "Este é o conteúdo do modal.",
-  headerContent: <h3 className="text-2xl font-bold">Cabeçalho Customizado</h3>,
-  footerContent: (
-    <button
-      className="btn btn-primary"
-      onClick={() => alert("Ação no rodapé!")}
-    >
-      Ação no Rodapé
-    </button>
-  ),
+export const BaseExample = Template.bind({});
+BaseExample.args = {
+  size: "md",
+  overflow: "inside",
 };
 
-// Variações de tamanho
 export const Small = Template.bind({});
-Small.args = {
-  ...WithOpenButton.args,
-  size: "sm",
-};
+Small.args = { size: "sm" };
 
 export const Medium = Template.bind({});
-Medium.args = {
-  ...WithOpenButton.args,
-  size: "md",
-};
+Medium.args = { size: "md" };
 
 export const Large = Template.bind({});
-Large.args = {
-  ...WithOpenButton.args,
-  size: "lg",
-};
+Large.args = { size: "lg" };
 
 export const ExtraLarge = Template.bind({});
-ExtraLarge.args = {
-  ...WithOpenButton.args,
-  size: "xl",
-};
+ExtraLarge.args = { size: "xl" };
 
 export const FullScreen = Template.bind({});
-FullScreen.args = {
-  ...WithOpenButton.args,
-  size: "full",
-};
+FullScreen.args = { size: "full" };
 
 export const OverflowInside = Template.bind({});
 OverflowInside.args = {
   size: "md",
   overflow: "inside",
-  title: "Modal com Overflow Inside",
-  children: (
-    <div style={{ height: "200vh" }}>
-      <p>Conteúdo muito longo dentro da modal.</p>
-    </div>
-  ),
 };
 
 export const OverflowOutside = Template.bind({});
 OverflowOutside.args = {
   size: "lg",
   overflow: "outside",
-  title: "Modal com Overflow Outside",
   children: (
     <div style={{ height: "100vh" }}>
-      <p>Conteúdo muito longo, mas o scroll é na página.</p>
+      <p>Conteúdo longo, mas o scroll está na página.</p>
     </div>
   ),
 };
